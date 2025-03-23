67a4183d59f9bd55513c981e0ac7fb09fb0c919f
m4rc310 <mlsilva@coamo.com.br>
2025-03-23 12:37:37 -0300

    fixed bug auth controll access

--- br-com-m4rc310-core-graphql/src/main/java/br/com/m4rc310/core/graphql/configurations/security/MAuthInterceptor.java
+++ br-com-m4rc310-core-graphql/src/main/java/br/com/m4rc310/core/graphql/configurations/security/MAuthInterceptor.java
@@ -1,5 +1,7 @@
 package br.com.m4rc310.core.graphql.configurations.security;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Collections;
@@ -16,6 +18,8 @@ import br.com.m4rc310.core.graphql.exceptions.MException;
 import br.com.m4rc310.core.graphql.mappers.annotations.MAuth;
 import io.leangen.graphql.ExtensionProvider;
 import io.leangen.graphql.GeneratorConfiguration;
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.execution.InvocationContext;
 import io.leangen.graphql.execution.ResolverInterceptor;
 import io.leangen.graphql.execution.ResolverInterceptorFactory;
@@ -29,15 +33,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Configuration
 public class MAuthInterceptor implements ResolverInterceptor {
-	
-	
+
 	/**
 	 * MAuthInterceptor
 	 */
 	public MAuthInterceptor() {
 	}
-	
-	
+
 	/**
 	 * Custom interceptors.
 	 *
@@ -52,7 +54,7 @@ public class MAuthInterceptor implements ResolverInterceptor {
 	/**
 	 * Around invoke.
 	 *
-	 * @param context the context
+	 * @param context      the context
 	 * @param continuation the continuation
 	 * @return the object
 	 * @throws Exception the exception
@@ -61,22 +63,28 @@ public class MAuthInterceptor implements ResolverInterceptor {
 	public Object aroundInvoke(InvocationContext context, Continuation continuation) throws Exception {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+		Method method = (Method) context.getResolver().getExecutable().getDelegate();
+	    if (isGetter(method)) {
+	        return continuation.proceed(context);
+	    }
+
 		if (Objects.nonNull(authentication)) {
 			MUserPrincipal principal = (MUserPrincipal) authentication.getPrincipal();
 			boolean isBasicToken = principal == null ? false
 					: principal.getAuthorities().stream().anyMatch(aa -> "basic".equalsIgnoreCase(aa.getAuthority()));
-			
+
 			boolean isAuthToken = principal == null ? false
 					: principal.getAuthorities().stream().anyMatch(aa -> "auth".equalsIgnoreCase(aa.getAuthority()));
 
 			MAuth auth = context.getResolver().getExecutable().getDelegate().getAnnotation(MAuth.class);
-			
+
 			if (Objects.isNull(auth) && isBasicToken) {
 				throw getWebException(401, "Access unauthorizade. User with basic privileges!");
-			}else if (Objects.isNull(auth) && isAuthToken) {
-				throw getWebException(401, "Access unauthorizade. User with auth privileges!");				
+			} else if (Objects.isNull(auth) && isAuthToken) {
+				throw getWebException(401, "Access unauthorizade. User with auth privileges!");
 			} else if (Objects.nonNull(auth)) {
-				if (isBasicToken && !Arrays.asList(auth.roles()).stream().anyMatch(aa -> "basic".equalsIgnoreCase(aa))) {
+				if (isBasicToken
+						&& !Arrays.asList(auth.roles()).stream().anyMatch(aa -> "basic".equalsIgnoreCase(aa))) {
 					throw getWebException(401, "Access unauthorizade. User with basic privileges!");
 				}
 
@@ -92,7 +100,7 @@ public class MAuthInterceptor implements ResolverInterceptor {
 					}
 					return false;
 				});
-				
+
 				if (!isAuth) {
 					throw getWebException(401, "Access unauthorizade.");
 				}
@@ -104,11 +112,43 @@ public class MAuthInterceptor implements ResolverInterceptor {
 	}
 
 	/**
+	 * isGetter
+	 * 
+	 */
+	private boolean isGetter(Method method) {
+	    String methodName = method.getName();
+	    
+	    if (hasGraphQLAnnotations(method)) {
+	        return false;
+	    }
+
+	    boolean startsWithGetOrIs = methodName.startsWith("get") || methodName.startsWith("is");
+	    boolean hasNoParameters = method.getParameterCount() == 0;
+	    boolean hasReturnValue = !void.class.equals(method.getReturnType());
+	    boolean isBooleanGetter = methodName.startsWith("is") && method.getReturnType().equals(boolean.class);
+
+	    return startsWithGetOrIs && hasNoParameters && (hasReturnValue || isBooleanGetter);
+	}
+
+	/**
+	 * hasGraphQLAnnotations
+	 */
+	private boolean hasGraphQLAnnotations(Method method) {
+	    for (Annotation annotation : method.getAnnotations()) {
+	        if (annotation.annotationType().equals(GraphQLMutation.class) ||
+	            annotation.annotationType().equals(GraphQLQuery.class)) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+
+	/**
 	 * Gets the web exception.
 	 *
-	 * @param code the code
+	 * @param code    the code
 	 * @param message the message
-	 * @param args the args
+	 * @param args    the args
 	 * @return the web exception
 	 */
 	private MException getWebException(int code, String message, Object... args) {
