0059a05cb01fe298a4f1af05ac35e9add08dd2af
m4rc310 <mlsilva@coamo.com.br>
2025-04-02 12:05:12 -0300

    ...

--- br-com-m4rc310-core-mqtt/src/main/java/br/com/m4rc310/core/mqtt/services/MMqttService.java
+++ br-com-m4rc310-core-mqtt/src/main/java/br/com/m4rc310/core/mqtt/services/MMqttService.java
@@ -4,7 +4,6 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.m4rc310.core.mqtt.constants.IConst;
@@ -25,18 +24,26 @@ public class MMqttService implements IConst{
 	@Value(PROPS_MQTT_SECURITY_PSWD_VALUE)
 	private String MQTT_PSWD;
 	
-	@Bean
-	MqttClient connect() throws Exception {
-		MqttClient client = new MqttClient(MQTT_API_URI, MQTT_USER);
-        MqttConnectOptions options = new MqttConnectOptions();
-        options.setCleanSession(true);
-        options.setUserName(MQTT_USER);
-        options.setPassword(MQTT_PSWD.toCharArray());
-        client.connect(options);
-        return client;
+	private MqttClient client;
+	
+	public MqttClient getClient() throws Exception{
+		return getClient(MQTT_API_URI);
 	}
 	
-	
+	public MqttClient getClient(String url) throws Exception{
+		if (client != null & client.isConnected()) {
+			return client;
+		}
+		
+		MqttConnectOptions options = new MqttConnectOptions();
+		options.setCleanSession(true);
+		options.setUserName(MQTT_USER);
+		options.setPassword(MQTT_PSWD.toCharArray());
+		
+		client = new MqttClient(url, MQTT_USER);
+		client.connect(options);
+		return client;
+	}	
 	
 	
 	
